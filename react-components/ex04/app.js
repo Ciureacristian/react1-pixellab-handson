b7ab3bce269dc620aebe92da30f2d7429c7c7582
Ciurea Cristian <123569444+Ciureacristian@users.noreply.github.com>
2023-11-21 09:27:10 -1000

    ex02-04

--- react-components/ex04/app.js
+++ react-components/ex04/app.js
@@ -26,6 +26,46 @@ const person = {
     },
   },
 };
+console.warn(`
+Folosind Object.entries() pe proprietatea skills, afiseaza abilitatile
+ persoanei daca acestea sunt true. Folosind propozitii de forma:
+ “person.name cunoaste: html.” “person.name cunoaste: javaScript.”`);
 
+const entries1 = Object.entries(person.skills);
+entries1.forEach((skillEntry) => {
+  const [skillId, isKnow] = skillEntry;
 
+  console.log(
+    `${person.name} ${isKnow ? 'cunoaste' : 'nu cunoaste'} ${skillId}.`,
+  );
+});
 
+console.warn(`
+Prin aceeasi metoda, afiseaza o lista inversata cu numele complet inversat al prietenilor.
+`);
+const reverseFriendsList = Object.entries(person.friends).reverse();
+reverseFriendsList.forEach((friendEntry) => {
+  const [, friend] = friendEntry;
+  const { surname, name } = friend;
+
+  console.log(`${surname} ${name}`);
+});
+
+console.warn(`
+Afiseaza propozitia: “Prietenii mei sunt Larry, Steven si Carol.” folosind Object.entries()
+`);
+console.log(
+  Object.entries(person.friends).reduce(
+    (message, friendEntry, index, friends) => {
+      const length = friends.length;
+      const [, { name }] = friendEntry;
+      const punctuation =
+        length - 1 === index ? '.' : length - 2 === index ? ' si ' : ', ';
+
+      message += `${name} ${punctuation}`;
+
+      return message;
+    },
+    'Prietenii mei sunt ',
+  ),
+);
